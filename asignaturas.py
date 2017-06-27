d35c61c9514b3a392157d5e1aff8c4979b4d0bc8
Jonathan Salinas <a.jonathan.sl@gmail.com>
2017-06-27 11:42:50 -0500

    Archivo de configuración, máximo de intentos

--- asignaturas.py
+++ asignaturas.py
@@ -1,42 +1,71 @@
 # -*- coding: utf-8 -*-
 from bs4 import BeautifulSoup
 import urllib2
-import time
+import logging
+from config import *
 
-archivo = open('CARPETA/archivo.txt', 'w')
+archivo = open("resultados/"+ARCHIVO_DESTINO, 'w')
+logging.basicConfig(filename='error.log')
 
+for x in CLAVES:
+    # Intenta hacer la petición
+    intentos = 0
+    contenido = ""
+    while intentos < MAXIMO_INTENTOS:
+        try:
+            contenido = urllib2.urlopen(URL_INSCRIPCIONES + str(x)).read()
+            break
+        except Exception, e:
+            intentos += 1
+            continue
 
-for x in xrange(0,10000):
-	contenido = urllib2.urlopen("http://inscripciones.ingenieria.unam.mx/consulta_horarios/index.php/horarios/consulta/"+str(x)).read()
-	soup = BeautifulSoup(contenido, 'html.parser')
+    # Valida máximo de intentos
+    if intentos >= MAXIMO_INTENTOS:
+        error = str(x) + ": Máximo de intentos - " + str(MAXIMO_INTENTOS)
+        logging.error(error)
+        continue
 
-	tables = soup.findAll('table')
-	
-	for rows in tables:
-		for tr in rows:
-			if len(tr) > 1:
-				cols = tr.findAll('td')
-				if len(cols) > 1:
-					vacantes_dos = cols[3].string.encode('utf-8')
-					clave = cols[0].string.encode('utf-8')
-					asignatura = cols[1].string.encode('utf-8')
-					grupo = cols[2].string.encode('utf-8')
-					vacantes = cols[3].string.encode('utf-8')
-					profesor = cols[4].string.encode('utf-8')
+    soup = BeautifulSoup(contenido, 'html.parser')
+    tables = soup.findAll('table')
 
-					inicio = cols[5].string.encode('utf-8')
-					fin = cols[6].string.encode('utf-8')
+    for rows in tables:
+        for tr in rows:
+            if len(tr) > 1:
+                cols = tr.findAll('td')
+                if len(cols) > 1:
+                    vacantes_dos = cols[3].string.encode('utf-8')
+                    clave = cols[0].string.encode('utf-8')
+                    asignatura = cols[1].string.encode('utf-8')
+                    grupo = cols[2].string.encode('utf-8')
+                    vacantes = cols[3].string.encode('utf-8')
+                    profesor = cols[4].string.encode('utf-8')
 
-					lunes = cols[7].center.contents[0].encode('utf-8')
-					martes = cols[8].center.contents[0].encode('utf-8')
-					miercoles = cols[9].center.contents[0].encode('utf-8')
-					jueves = cols[10].center.contents[0].encode('utf-8')
-					viernes = cols[11].center.contents[0].encode('utf-8')
-					sabado = cols[12].center.contents[0].encode('utf-8')
+                    inicio = cols[5].string.encode('utf-8')
+                    fin = cols[6].string.encode('utf-8')
 
-					tipo = cols[13].string.encode('utf-8')
+                    lunes = cols[7].center.contents[0].encode('utf-8')
+                    martes = cols[8].center.contents[0].encode('utf-8')
+                    miercoles = cols[9].center.contents[0].encode('utf-8')
+                    jueves = cols[10].center.contents[0].encode('utf-8')
+                    viernes = cols[11].center.contents[0].encode('utf-8')
+                    sabado = cols[12].center.contents[0].encode('utf-8')
 
-					archivo.write(str(x)+"#"+clave+"#"+asignatura+"#"+tipo+"#"+grupo+"#"+profesor+"#"+inicio+"#"+fin+"#"+lunes+"#"+martes+"#"+miercoles+"#"+jueves+"#"+viernes+"#"+sabado+"#"+"\n")
-	print str(x)
-	
+                    tipo = cols[13].string.encode('utf-8')
+
+                    archivo.write(str(x)
+                                  + TOKEN + clave
+                                  + TOKEN + asignatura
+                                  + TOKEN + tipo
+                                  + TOKEN + grupo
+                                  + TOKEN + profesor
+                                  + TOKEN + inicio
+                                  + TOKEN + fin
+                                  + TOKEN + lunes
+                                  + TOKEN + martes
+                                  + TOKEN + miercoles
+                                  + TOKEN + jueves
+                                  + TOKEN + viernes
+                                  + TOKEN + sabado
+                                  + TOKEN + "\n")
+    print str(x)
 archivo.close()
---
+++ config.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# Si existe se sobreescribe, sino se crea
+ARCHIVO_DESTINO = "JUNIO_2017"
+
+# Máximo de intentos por petición
+MAXIMO_INTENTOS = 3
+
+# URL para obtener las materias
+URL_INSCRIPCIONES = "http://inscripciones.ingenieria.unam.mx/consulta_horarios/index.php/horarios/consulta/"
+
+# Separador del STR
+TOKEN = "#"
+
+# Lista [] de claves a buscar
+CLAVES = range(0, 10000)
